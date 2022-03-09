dea8c0462c1a20f841b643472ad902ad7b544603
pcancio <pcancio@gmail.com>
2022-03-08 20:45:22 -0500

    started components

---
+++ src/components/About/index.js
@@ -0,0 +1,12 @@
+import { getAllByPlaceholderText } from "@testing-library/react";
+import React from "react";
+
+function About() {
+  return (
+    <section>
+      <h1 id="about">About Me</h1>
+    </section>
+  );
+}
+
+export default About;

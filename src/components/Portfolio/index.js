8e23af27b9a392534cfccb2cba6d611044b9452a
pcancio <pcancio@gmail.com>
2022-03-09 22:24:38 -0500

    loaded portfolio items

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { Card } from "react-bootstrap";
 import LetsEat from "../../assets/LetsEat.png";
-// import budgettracker from "../../assets/budgettracker.png";
-// import DayPlanner from "../../assets/DayPlanner.png";
-// import PasswordGenerator from "../../assets/PasswordGenerator.png";
-// import PetProject from "../../assets/PetProject.png";
-// import TechBlog from "../../assets/TechBlog.png";
+import PetProject from "../../assets/PetProject.png";
+import budgettracker from "../../assets/budgettracker.png";
+import DayPlanner from "../../assets/DayPlanner.png";
+import PasswordGenerator from "../../assets/PasswordGenerator.png";
+import TechBlog from "../../assets/TechBlog.png";
 
 function Portfolio() {
   return (
@@ -38,6 +38,164 @@ function Portfolio() {
                 </Card.Link>
                 <Card.Link
                   href="https://github.com/a-breezy/LetsEat"
+                  target="_blank"
+                >
+                  Github
+                </Card.Link>
+              </Card.Body>
+            </Card>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col-lg-4 py-2">
+            <Card style={{ width: "18rem" }}>
+              <Card.Img variant="top" src={PetProject} alt="Pet Project" />
+              <Card.Body>
+                <Card.Title>Pet Project</Card.Title>
+                <Card.Text>
+                  Pet Project is an online community for pet owners to post
+                  about their pets, allergies, diets and more. Users can comment
+                  on other posts and will receive notifications when other users
+                  comment on their posts.
+                </Card.Text>
+                <Card.Link
+                  href="https://github.com/mkohn4/pet-project"
+                  target="_blank"
+                >
+                  Deployed App
+                </Card.Link>
+                <Card.Link
+                  href="https://warm-springs-61498.herokuapp.com/"
+                  target="_blank"
+                >
+                  Github
+                </Card.Link>
+              </Card.Body>
+            </Card>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col-lg-4 py-2">
+            <Card style={{ width: "18rem" }}>
+              <Card.Img
+                variant="top"
+                src={budgettracker}
+                alt="Budget Tracker"
+              />
+              <Card.Body>
+                <Card.Title>Budget Tracker</Card.Title>
+                <Card.Text>
+                  This is a PWA made with MongoDB that allows users to be able
+                  to log and track their withdrawals and deposits and keep their
+                  balance accurate with or without a data or internet
+                  connection.
+                </Card.Text>
+                <Card.Link
+                  href="https://secure-sea-09970.herokuapp.com/"
+                  target="_blank"
+                >
+                  Deployed App
+                </Card.Link>
+                <Card.Link
+                  href="https://github.com/pcancio/PWA-Budget-Tracker"
+                  target="_blank"
+                >
+                  Github
+                </Card.Link>
+              </Card.Body>
+            </Card>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col-lg-4 py-2">
+            <Card style={{ width: "18rem" }}>
+              <Card.Img
+                variant="top"
+                src={DayPlanner}
+                alt="Work Day Scheduler"
+              />
+              <Card.Body>
+                <Card.Title>Work Day Scheduler</Card.Title>
+                <Card.Text>
+                  This is a simple daily calendar app that allows users to
+                  schedule their day. Each time block is color-coded to indicate
+                  whether the event or meeting is in the past, present, or
+                  future. The data is saved in local storage and persists even
+                  after reloading the page.
+                </Card.Text>
+                <Card.Link
+                  href="https://pcancio.github.io/daily-planner/"
+                  target="_blank"
+                >
+                  Deployed App
+                </Card.Link>
+                <Card.Link
+                  href="https://github.com/pcancio/daily-planner"
+                  target="_blank"
+                >
+                  Github
+                </Card.Link>
+              </Card.Body>
+            </Card>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col-lg-4 py-2">
+            <Card style={{ width: "18rem" }}>
+              <Card.Img
+                variant="top"
+                src={PasswordGenerator}
+                alt="Password Generator"
+              />
+              <Card.Body>
+                <Card.Title>Password Generator</Card.Title>
+                <Card.Text>
+                  This is an app that generates a random password. The app runs
+                  in the browser and features dynamically updated HTML and CSS
+                  powered by JavaScript code. It has a clean and polished user
+                  interface and is responsive, which ensures that it adapts to
+                  multiple screen sizes.
+                </Card.Text>
+                <Card.Link
+                  href="https://pcancio.github.io/Password-Generator/"
+                  target="_blank"
+                >
+                  Deployed App
+                </Card.Link>
+                <Card.Link
+                  href="https://github.com/pcancio/Password-Generator"
+                  target="_blank"
+                >
+                  Github
+                </Card.Link>
+              </Card.Body>
+            </Card>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col-lg-4 py-2">
+            <Card style={{ width: "18rem" }}>
+              <Card.Img variant="top" src={TechBlog} alt="Tech Blog" />
+              <Card.Body>
+                <Card.Title>Tech Blog</Card.Title>
+                <Card.Text>
+                  This is an app for techies. Login and authetication is
+                  required to join the blog. Once signed up and logged in, users
+                  can post, comment, and interace with other techies!
+                </Card.Text>
+                <Card.Link
+                  href="https://nameless-headland-84483.herokuapp.com/"
+                  target="_blank"
+                >
+                  Deployed App
+                </Card.Link>
+                <Card.Link
+                  href="https://github.com/pcancio/my-awesome-techie-blog"
                   target="_blank"
                 >
                   Github
